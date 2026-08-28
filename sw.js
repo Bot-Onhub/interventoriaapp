a40f62685463f57daa61115e3b513f2b2c66225f
Bot-Onhub <botonhub@gmail.com>
2026-08-27 20:47:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aaas-campo-v2'; // Cambiamos la versión para forzar actualización
+const CACHE_NAME = 'aaas-campo-v3'; // Cambiamos la versión para forzar actualización
 
 // SOLO archivos locales reales. Si falta uno, la caché colapsa.
 const urlsToCache = [
